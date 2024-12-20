af999d8278909799cc89ba7eb6b51d9f03aea186
YovanW <141811920+YovanW@users.noreply.github.com>
2024-12-20 09:01:44 +0700

    Update and rename graph_yovan to graph_yovan.py

--- graph_yovan.py
+++ graph_yovan.py
@@ -1,22 +1,4 @@
-class Node:
-    def __init__(self, judul, artist, link, genre):
-        self.judul = judul
-        self.artist = artist
-        self.link = link
-        self.genre = genre
-        self.next = None
 
-    def get_judul(self):
-        return self.judul
-
-    def get_artist(self):
-        return self.artist
-
-    def get_link(self):
-        return self.link
-
-    def get_genre(self):
-        return self.genre
 
 class MusicGraph:
     def __init__(self):
