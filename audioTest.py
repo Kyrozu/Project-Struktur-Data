aa5af424450a2c7a375cf7941266cc092a352a21
Furozu <nat80222@gmail.com>
2024-12-31 13:50:36 +0700

    added import fail by pass

--- audioTest.py
+++ audioTest.py
@@ -3,8 +3,14 @@ install libary pemutar lagu lewat link youtube
 pip install yt-dlp python-vlc
 """
 
-import vlc
-import yt_dlp as youtube_dl
+try:
+    import vlc
+    import yt_dlp as youtube_dl
+except:
+    # jika import vlc atau yt_dlp fail 
+    vlc = None
+    youtube_dl = None
+
 import time
 
 # Nathanael / C14230178
@@ -15,49 +21,54 @@ class YouTubeAudioPlayer:
         self.player = None
 
     def play(self, youtube_url):
-        # Setting untuk extract audio dari YouTube
-        ydl_opts = {
-            'format': 'bestaudio/best',  # Audio format
-            'noplaylist': True,          # Tidak download playlists
-            'quiet': True,               # Tidak output yang tidak perlu
-            'no_warnings': True,         # Tidak ada warning
-        }
+        # kalau vlc atau yt_dlp fail skip play audio 
+        if  vlc != None and youtube_dl != None:
 
-        # Extract audio dari URL YouTube nya
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            info = ydl.extract_info(youtube_url, download=False)
-            audio_url = info.get('url', None)
+            # Setting untuk extract audio dari YouTube
+            ydl_opts = {
+                'format': 'bestaudio/best',  # Audio format
+                'noplaylist': True,          # Tidak download playlists
+                'quiet': True,               # Tidak output yang tidak perlu
+                'no_warnings': True,         # Tidak ada warning
+            }
 
-            if audio_url:
-                # Initialize VLC nya
-                instance = vlc.Instance()
-                self.player = instance.media_player_new()
-                media = instance.media_new(audio_url)
-                self.player.set_media(media)
-                
-                self.player.audio_set_volume(self.initial_volume)  # Setting volume
-                self.player.play()
+            # Extract audio dari URL YouTube nya
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                info = ydl.extract_info(youtube_url, download=False)
+                audio_url = info.get('url', None)
 
-                # Loop untuk perubahan player state
-                while not self.isPlaying():
-                    time.sleep(0.5)         # Check setiap 0.5 detik
+                if audio_url:
+                    # Initialize VLC nya
+                    instance = vlc.Instance()
+                    self.player = instance.media_player_new()
+                    media = instance.media_new(audio_url)
+                    self.player.set_media(media)
+                    
+                    self.player.audio_set_volume(self.initial_volume)  # Setting volume
+                    self.player.play()
 
-                # # untuk player yang berjalan bersamaan dengan menu tapi terpisah
-                # while True:
-                #     state = self.player.get_state()
+                    # Loop untuk perubahan player state
+                    while not self.isPlaying():
+                        time.sleep(0.5)         # Check setiap 0.5 detik
 
-                #     # TODO: harus di pindah
-                #     self.sync.control_input()
+                    # # untuk player yang berjalan bersamaan dengan menu tapi terpisah
+                    # while True:
+                    #     state = self.player.get_state()
 
-                #     # Berhenti jika musik selesai atau error
-                #     if state in [vlc.State.Ended, vlc.State.Error]:
-                #         print("Music finished")
-                #         break
+                    #     # TODO: harus di pindah
+                    #     self.sync.control_input()
 
-                #     # Berhenti jika playlist habis
-                #     if self.sync.get_queue().isEmpty():
-                #         print("Playlist empty. Stopping playback.")
-                #         break
+                    #     # Berhenti jika musik selesai atau error
+                    #     if state in [vlc.State.Ended, vlc.State.Error]:
+                    #         print("Music finished")
+                    #         break
+
+                    #     # Berhenti jika playlist habis
+                    #     if self.sync.get_queue().isEmpty():
+                    #         print("Playlist empty. Stopping playback.")
+                    #         break
+
+
 
     def stop(self):
         if self.player:
